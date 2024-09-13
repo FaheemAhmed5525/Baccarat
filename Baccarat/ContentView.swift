b96833eff9c912e6d48d91e277f314602734ecd5
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-09-13 16:58:59 +0500

    Main Logic completed

--- Baccarat/ContentView.swift
+++ Baccarat/ContentView.swift
@@ -78,71 +78,7 @@ struct ContentView: View {
                                 Text("Betting Amount: \(bettingAmount)$")
                                 
                                 Button("Start") {
-                                    print("drawing cards")
-                                    var randomNum: Int
-                                    
-                                    randomNum = Int.random(in: 1...52)
-                                    
-                                    //get a unused card
-                                    while card[randomNum] == true {
-                                        print("Finding free card. Randome number\(randomNum)")
-                                        randomNum = Int.random(in: 1...52)
-                                    }
-                                    print("Found free card. Randome number\(randomNum)")
-                                    //place first card on player hand
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        playerHandCard1 = randomNum
-                                    }
-                                    
-                                    card[randomNum] = true
-                                    
-                                    
-                                    
-                                    //get a unused card
-                                    while card[randomNum] == true {
-                                        print("Finding free card. Randome number\(randomNum)")
-                                        randomNum = Int.random(in: 1...52)
-                                    }
-                                    print("Found free card. Randome number\(randomNum)")
-                                    //place first card on banker hand
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        bankerHandCard1 = randomNum
-                                    }
-                                    card[randomNum] = true
-                                    
-                                    
-                                    //get a unused card
-                                    while card[randomNum] == true {
-                                        print("Finding free card. Randome number\(randomNum)")
-                                        randomNum = Int.random(in: 1...52)
-                                    }
-                                    print("Found free card. Randome number\(randomNum)")
-                                    //place second card on player hand
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        playerHandCard2 = randomNum
-                                    }
-                                    card[randomNum] = true
-                                    
-                                    
-                                    
-                                    //get a unused card
-                                    while card[randomNum] == true {
-                                        print("Finding free card. Randome number\(randomNum)")
-                                        randomNum = Int.random(in: 1...52)
-                                    }
-                                    print("Found free card. Randome number\(randomNum)")
-                                    //place second card on banker hand
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        playerHandCard1 = randomNum
-                                    }
-                                    card[randomNum] = true
-                                    
-                                    print("Player Card 1: \(playerHandCard1)")
-                                    print("Player Card 2: \(playerHandCard2)")
-                                    print("Player Card 3: \(playerHandCard3)")
-                                    print("Banker Card 1: \(bankerHandCard1)")
-                                    print("Banker Card 2: \(bankerHandCard2)")
-                                    print("Banker Card 3: \(bankerHandCard3)")
+                                    drawCards()
                                 }
                                 .padding(.bottom, 0)
                                 .background(Color.cyan)
@@ -162,7 +98,6 @@ struct ContentView: View {
                                 // PlayerHand
                             PlayerHandView(width: width/5, height: height/4)
                                 .overlay(
-                                    
                                     HStack(alignment: .center, spacing: -width/10) {
                                         Image("Card\(playerHandCard1)")
                                              .resizable()
@@ -186,7 +121,7 @@ struct ContentView: View {
                             BankerHandView(width: width/5, height: height/4)
                                 .overlay (
                                     HStack(alignment: .center, spacing: -width/10) {
-                                        Image("Card\(bankerHandCard1)")
+                                        Image("Card\(bankerHandCard3)")
                                              .resizable()
                                              .frame(width: width/10, height: height/4)
                                              .cornerRadius(8)
@@ -196,7 +131,7 @@ struct ContentView: View {
                                               .frame(width: width/10, height: height/4)
                                               .cornerRadius(8)
                                               .padding(.leading, 24)
-                                         Image("Card\(bankerHandCard3)")
+                                         Image("Card\(bankerHandCard1)")
                                               .resizable()
                                               .frame(width: width/10, height: height/4)
                                               .cornerRadius(8)
@@ -209,6 +144,8 @@ struct ContentView: View {
                         Spacer()
                         BettingAmountChangeButtons
                     }
+                    
+                    
                     
                     //let x = geometry.frame(in: .global).origin.x
                     let y = geometry.frame(in: .global).origin.y
@@ -370,7 +307,134 @@ struct ContentView: View {
 
     }
 
+    func drawCards() {
+        print("drawing cards")
+        
+        //get a unused card
+        //place first card on player hand
+        playerHandCard1 = drawNewCard()
+        
+        //get a unused card
+        //place first card on player hand
+        playerHandCard2 = drawNewCard()
+        
+        //get a unused card
+        //place second card on banker hand
+        bankerHandCard1 = drawNewCard()
+        
+        //get a unused card
+        //place second card on banker hand
+        bankerHandCard2 = drawNewCard()
+        
+        
+        print("Player Card 1: \(playerHandCard1)")
+        print("Player Card 2: \(playerHandCard2)")
+        print("Player Card 3: \(playerHandCard3)")
+        print("Banker Card 1: \(bankerHandCard1)")
+        print("Banker Card 2: \(bankerHandCard2)")
+        print("Banker Card 3: \(bankerHandCard3)")
+        
+        dealCards()
+    }
     
+    /// draw card is called to draw a new card for cardShoe
+    func drawNewCard()-> Int {
+        
+        var randomNum = Int.random(in: 1...52)
+        
+        //get a unused card
+        while card[randomNum] == true {
+            print("Number failed: \(randomNum)")
+            randomNum = Int.random(in: 1...52)
+        }
+        
+        //place first card on player hand
+        card[randomNum] = true
+        
+        return randomNum
+    }
+    
+    
+    
+    
+    
+    /// dealCards  function is executed after the initial cards are drawn
+    func dealCards() {
+        var playerTotal: Int = ((playerHandCard1 % 13) + (playerHandCard2 % 13)) % 10
+        var bankerTotal: Int = ((bankerHandCard1 % 13) + (bankerHandCard2 % 13)) % 10
+        
+        //if the care is tie
+        if playerTotal == bankerTotal {
+            handleTie()
+            return
+        }
+        else if playerTotal == 8 || playerTotal == 9 {
+            if playerTotal > bankerTotal {
+                handlePlayerWin()
+                return
+            }
+            
+        }
+        else if bankerTotal == 8 || bankerTotal == 9 {
+            if bankerTotal > playerTotal {
+                handleBankerWin()
+                return
+            }
+        }
+        else if playerTotal <= 5 {
+            
+            //draws a third card
+            playerHandCard3 = drawNewCard()
+            playerTotal = ((playerHandCard1 % 13) + (playerHandCard2 % 13) + (playerHandCard3) % 13) % 10
+            
+        }
+        let playerThirdCard = ((playerHandCard2) % 13) % 10
+        
+        //Condition when banker draws third card
+        if (bankerTotal >= 0 && bankerTotal <= 2) ||
+            (bankerTotal == 3 && playerTotal != 8 ) ||
+            (bankerTotal == 4 && (playerThirdCard >= 2 && playerThirdCard <= 7)) ||
+            (bankerTotal == 5 && (playerThirdCard >= 4 && playerThirdCard <= 7)) ||
+            (bankerTotal == 6 && (playerThirdCard == 6 || playerThirdCard == 7)) {
+            
+            bankerHandCard3 = drawNewCard()
+            
+        bankerTotal = ((bankerHandCard1 % 13) + (bankerHandCard2 % 13) + (bankerHandCard3 % 13)) % 10
+        }
+        
+        //if the case is tie
+        if playerTotal == bankerTotal {
+            handleTie()
+            
+        }
+        // player won
+        else if playerTotal > bankerTotal {
+            handlePlayerWin()
+            
+        }
+        // banker won
+        else {
+            handleBankerWin()
+        }
+        
+    }
+    
+    
+    // handleTie is called where tie occurs while dealing cards
+    func handleTie() {
+        print("It tie!")
+    }
+    
+    // handlePlayerWin() is called where the player wins
+    func handlePlayerWin() {
+        print("Player won the game")
+    }
+    
+    // handleBankerWin() is called where the banker wins
+    func handleBankerWin() {
+        print("Banker won the game")
+        
+    }
     /// Betting area for tie
     func TieAreaView(forPlayer: Int, center: CGPoint, radius: CGFloat, startAngle: Angle, endAngle: Angle) -> some View {
 //
